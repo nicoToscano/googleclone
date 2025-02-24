04ed923eef3443e4b0950c1f3e3be501e14a713a
Nicolás Toscano <nicolas.toscano@esoft.ec>
2025-02-24 18:26:06 -0500

    footer section

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -11,33 +11,34 @@ export function Footer() {
                 <div className='flex flex-row items-center'>
                     <TextUnderline
                     className='px-2'
-                    nameDirection="Sobre Google"/>
+                    nameDirection="Sobre Google"
+                    urlDirection='https://about.google/?utm_source=google-EC&utm_medium=referral&utm_campaign=hp-footer&fg=1'/>
                     <TextUnderline
                     className='px-2'
                     nameDirection="Publicidad"
-                    urlDirection='https://www.google.com/intl/es-419_ar/about/products/' />
+                    urlDirection='https://ads.google.com/intl/es-419/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1' />
                     <TextUnderline
                     className='px-2'
                     nameDirection="Negocios"
-                    urlDirection='https://www.google.com/services/' />
+                    urlDirection='https://www.google.com/business/?subid=ww-ww-et-g-awa-a-g_hpbfoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpbfooter&fg=1' />
                     <TextUnderline
                     className='px-2'
                     nameDirection="Cómo funciona la Búsqueda"
-                    urlDirection='https://www.google.com/search/howsearchworks/' />
+                    urlDirection='https://www.google.com/search/howsearchworks/?fg=1' />
                 </div>
                 <div className='flex flex-row items-center'>
                     <TextUnderline
                     className='px-2'
                     nameDirection="Privacidad"
-                    urlDirection='https://policies.google.com/privacy?hl=es-419&gl=ar' />
+                    urlDirection='https://policies.google.com/privacy?hl=es-419&fg=1' />
                     <TextUnderline
                     className='px-2'
                     nameDirection="Condiciones"
-                    urlDirection='https://policies.google.com/terms?hl=es-419&gl=ar' />
+                    urlDirection='https://policies.google.com/terms?hl=es-419&fg=1' />
                     <TextUnderline
                     className='px-2'
                     nameDirection="Configuración"
-                    urlDirection='https://www.google.com/preferences?hl=es-419&gl=ar' />
+                    urlDirection='https://policies.google.com/terms?hl=es-419&fg=1' />
                 </div>
             </div>            
         </footer>
