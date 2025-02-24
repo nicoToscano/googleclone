537b7f38fc6316918020e64ba020249bd8f48791
Nicolás Toscano <nicolas.toscano@esoft.ec>
2025-02-24 18:26:52 -0500

    search bar component

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,10 +1,27 @@
 import { Search } from 'lucide-react';
+import { useState } from 'react';
 
-export function SearchBar (){
+export function SearchBar() {
+
+    const [search, setSearch] = useState('')
+
+    const handleSearch = () => {
+        if (search) {
+            window.location.href = `https://www.google.com/search?q=${encodeURIComponent(search)}`;
+        }
+    };
+
     return (
         <div className="w-[580px] h-[44px] bg-white border border-gray-300 rounded-full flex items-center px-4 shadow-md">
-                <Search className='opacity-50 h-4 w-4'/>
-                <input className="w-full ml-2 text-gray-900 border-none focus:outline-none" type="text" placeholder="" />
+            <Search className='opacity-50 h-4 w-4' />
+            <input
+                className="w-full ml-2 text-gray-900 border-none focus:outline-none"
+                type="text"
+                placeholder=""
+                onClick={handleSearch}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={(e) => e.key === 'Enter' && handleSearch()} />
         </div>
     )
 }
