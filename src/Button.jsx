8c5a746bfcfbf57e73106ed1b43781e9e1247c8f
Nicolás Toscano <nicolas.toscano@esoft.ec>
2025-02-24 18:28:02 -0500

    update button with a

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,9 +1,9 @@
-export function Button({ nameButton, urlButton }) {
+export function Button1({ nameButton, urlButton }) {
   return (
     <div className="px-1.5 my-2.5">
-      <button className="bg-zinc-50 border border-zinc-50 hover:border-zinc-200 text-sm text-black font-extralight py-2 px-4 rounded h-9 min-w-14" href={urlButton}>
+      <a href={urlButton} className="bg-zinc-50 border border-zinc-50 hover:border-zinc-200 text-sm text-black font-extralight py-2 px-4 rounded h-9 min-w-14">
         {nameButton}
-      </button>
+      </a>
     </div>
   )
     
